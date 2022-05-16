73f29bf475686604ec63cf66f86112c0315ab330
Vishal Dogra <dogravishal1598@gmail.com>
2022-05-16 16:05:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
