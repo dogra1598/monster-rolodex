503b51f8eaccdbf08b7151129c42ae6f8c589f15
Vishal Dogra <dogravishal1598@gmail.com>
2022-05-17 14:36:04 +0530

    used js fetch function to get the monsters name and assign the json response to the monster array (property of the state object)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { Component } from "react";
+
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
+      return response
+        .json()
+        .then((users) => this.setState({ monsters: users }));
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
